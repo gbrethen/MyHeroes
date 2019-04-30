4158dd743930ba801f8ea41c8e1703ee1edd3d38
Greg Brethen <gbrethen@att.net>
2019-04-30 09:42:55 -0500

    Initial push of MyHeroes tutorial.

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+    { id: 11, name: 'Spider Man' },
+    { id: 12, name: 'Iron Man' },
+    { id: 13, name: 'The Hulk' },
+    { id: 14, name: 'Vision' },
+    { id: 15, name: 'Dr. Strange' },
+    { id: 16, name: 'Captain America' },
+    { id: 17, name: 'Captain Marvel' },
+    { id: 18, name: 'The Black Panther' },
+    { id: 19, name: 'Ant Man' },
+    { id: 20, name: 'Thor' }
+];
